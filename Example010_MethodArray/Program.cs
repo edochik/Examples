2cba1547fc8b118be62f06fde59e6e7827a46403
edochik <edoch@yandex.ru>
2022-04-16 17:31:57 +0300

    add Example010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] array = { 15, 22, 34, 4, 52, 64, 4, 628, 92};
+
+int n = array.Length;
+int find = 4;
+
+int index =0;
+
+while(index < n)
+{
+   if(array[index] == find)
+   {
+       Console.WriteLine(index);
+       break; // если несколько элементов в массиве одинаковых, находит первый индекс элемента и заканчивает операцию
+   }
+   //index = index + 1;
+    index++;
+
+
+}
