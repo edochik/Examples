5480c86d6c1f4eb59c3ed8b8aae9bd8195211372
edochik <edoch@yandex.ru>
2022-04-23 22:45:17 +0300

    modified Example011

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,17 +1,19 @@
-﻿void FillArray(int[] collection) // наполняет массив элементами
+﻿void FillArray(int[] collection)//метод создающий массив
 {
-    int lenght = collection.Length;
+    int length = collection.Length; // длина массива + название.Lenght
     int index = 0;
-    while (index < lenght)
+    while (index < length)
     {
-        collection[index] = new Random().Next(1, 10);
-        //index = index + 1;
+        collection[index] = new Random().Next(1, 10);// создать 10 случайных чисел
+        //index = index +1;
         index++;
 
     }
+
+
 }
 
-void PrintArray(int[] col)// Метод который печатает массив
+void PrintArray(int[] col) //метод печатающий массив void ни чего не возвращает
 {
     int count = col.Length;
     int position = 0;
@@ -23,28 +25,33 @@ void PrintArray(int[] col)// Метод который печатает масс
 
 }
 
-int IndexOf(int[] collection, int find)// 
+int IndexOf(int[] collection, int find) // находим нужный элемент в массиве
 {
-    int count = collection.Length;
+    int count = collection.Length;//длина местода первого
     int index = 0;
-    int position = 0;
+    int position = -1; // если мы в массиве ищем не существующи элемент то будет 0, чтобы этого не было надо поставить -1
     while (index < count)
     {
         if (collection[index] == find)
         {
             position = index;
-            break;
+            break; //если в массиве одинакова цифра на разных индекса, то это останавливает на первом элементе
         }
         index++;
-        return position;
+
     }
+    return position;
 }
 
-int[] array = new int[10]; // создай новый массив в котором будет 10 элементов (изначально он будет заполнен нулями)
+int[] array = new int[10];// создай массив в котором будет 10 элементов, нужно создать массив который его заполнит
 
-FillArray(array);//заполнил данный массив
-PrintArray(array);//распечатал массив
+FillArray(array); // обращение к методу заполняющему массив
+array[4] = 4;
+array[6] = 4;
+PrintArray(array); // обращение к методу распечатывающему массив
 Console.WriteLine();
 
-int pos = IndexOf(array, 4);
+int pos = IndexOf(array, 444);
 Console.WriteLine(pos);
+
+/// если искать элемент, который не сущестует, то позиция нидекса будет 0
